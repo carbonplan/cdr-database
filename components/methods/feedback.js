38c0dc2848440e34d5c32cba44217543fbf86486
freeman-lab <the.freeman.lab@gmail.com>
2020-04-20 00:33:07 -0700

    add email to feedback

--- components/methods/feedback.js
+++ components/methods/feedback.js
@@ -4,12 +4,16 @@ const Feedback = () => {
   return (
     <>
     <Text sx={{ mb: [2] }}>
-      Want to provide feedback on a metric, method, project, or this dashboard? Open an 
+      Want to provide feedback on a metric, method, or this dashboard? Open an 
       issue <Link href='https://github.com/carbonplan/reports'>here</Link>.
     </Text>
     <Text sx={{ mb: [2] }}>
       Want to provide feedback on our API or schema? Open an 
       issue <Link href='https://github.com/carbonplan/api'>here</Link>.
+    </Text>
+    <Text sx={{ mb: [2] }}>
+      Are you a project developer or funder and interested in our work? Email us at{' '}
+      <Link href='mailto:hello@carbonplan.org'>hello@carbonplan.org</Link>.
     </Text>
     </>
   )
