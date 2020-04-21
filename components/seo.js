2bee9b89f9abec8e0201ccc95e9deaa45f27801d
freeman-lab <the.freeman.lab@gmail.com>
2020-04-20 23:55:00 -0700

    use new favicon

--- components/seo.js
+++ components/seo.js
@@ -6,7 +6,7 @@ const Seo = () => (
     <meta name='description' content='Reports on carbon removal projects and technologies.' />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel='canonical' content='https://reports.carbonplan.org/' />
-    <link rel='icon' href='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/medium-black-circle_26ab.png'/>
+    <link rel='icon' href='/favicon.png'/>
   </Head>
 )
 
