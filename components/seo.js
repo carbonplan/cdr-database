7a34e402537137aa3405f1eb2c8919a7e5208db7
freeman-lab <the.freeman.lab@gmail.com>
2020-05-07 13:22:50 -0700

    fix seo

--- components/seo.js
+++ components/seo.js
@@ -5,11 +5,17 @@ const Seo = () => (
     <title>(carbon)plan reports</title>
     <meta name='description' content='Reports on carbon removal projects and technologies.' />
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    <link rel='canonical' content='https://reports.carbonplan.org/' />
-    <link rel='icon' href='/favicon.svg'/>
-    <link rel='apple-touch-icon' href='/favicon.png'/>
+    <link rel='canonical' content='https://carbonplan.org/'/>
+    <link rel='icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.svg'/>
+    <link rel='apple-touch-icon' href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.png'/>
     <meta property='og:title' content='(carbon)plan'/>
-    <meta property='og:image' content='/favicon.png'/>
+    <meta property="og:description" content="Data and science for carbon removal."/>
+    <meta property='og:image' content='https://carbonplan-assets.s3.amazonaws.com/images/social.png'/>
+    <meta property="og:url" content="https://carbonplan.org"/>
+    <meta name="twitter:title" content="(carbon)plan"/>
+    <meta name="twitter:description" content="Data and science for carbon removal."/>
+    <meta name="twitter:image" content="https://carbonplan-assets.s3.amazonaws.com/images/social.png"/>
+    <meta name="twitter:card" content="summary_large_image"/>
   </Head>
 )
 
