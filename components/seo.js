3581693eea514784878964c27d18834182db72b7
freeman-lab <the.freeman.lab@gmail.com>
2020-11-22 18:30:04 -0800

    update cards

--- components/seo.js
+++ components/seo.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 
 const Seo = ({ shareCard, shareDescription, shareTitle }) => (
   <Head>
-    <title>carbonplan / research</title>
+    <title>carbonplan / reports</title>
     <meta
       name='description'
       content={
         shareDescription
           ? shareDescription
-          : 'Datasets, models, interactives, and commentary on carbon removal and climate solutions.'
+          : 'Public database of reports on carbon removal projects and technologies.'
       }
     />
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
@@ -21,35 +21,35 @@ const Seo = ({ shareCard, shareDescription, shareTitle }) => (
       rel='apple-touch-icon'
       href='https://carbonplan-assets.s3.amazonaws.com/images/favicon.png'
     />
-    <meta property='og:title' content='carbonplan / research' />
+    <meta property='og:title' content='carbonplan / reports' />
     <meta
       property='og:description'
       content={
         shareDescription
           ? shareDescription
-          : 'Datasets, models, interactives, and commentary on carbon removal and climate solutions.'
+          : 'Public database of reports on carbon removal projects and technologies.'
       }
     />
     <meta
       property='og:image'
       content={`https://carbonplan-assets.s3.amazonaws.com/images/social/${
-        shareCard ? shareCard : 'research'
+        shareCard ? shareCard : 'reports'
       }.png`}
     />
     <meta property='og:url' content='https://carbonplan.org' />
-    <meta name='twitter:title' content='carbonplan / research' />
+    <meta name='twitter:title' content='carbonplan / reports' />
     <meta
       name='twitter:description'
       content={
         shareDescription
           ? shareDescription
-          : 'Datasets, models, interactives, and commentary on carbon removal and climate solutions.'
+          : 'Public database of reports on carbon removal projects and technologies.'
       }
     />
     <meta
       name='twitter:image'
       content={`https://carbonplan-assets.s3.amazonaws.com/images/social/${
-        shareCard ? shareCard : 'research'
+        shareCard ? shareCard : 'reports'
       }.png`}
     />
     <meta name='twitter:card' content='summary_large_image' />
