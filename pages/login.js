b6d7033e6502bc4b8c2eb772bee0c610b41bf235
freeman-lab <the.freeman.lab@gmail.com>
2021-01-04 01:11:56 -0800

    no footer on login

--- pages/login.js
+++ pages/login.js
@@ -42,7 +42,7 @@ function Login({ origin }) {
   }
 
   return (
-    <Layout status={status}>
+    <Layout status={status} footer={false}>
       <Heading sx={{ my: [4, 5, 5], fontSize: [6, 7, 7] }}>
         This page is private
       </Heading>
