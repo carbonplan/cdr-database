12acb803f975a90a828877fb54ba388c023d4495
freyachay <freyachay@gmail.com>
2021-02-24 09:15:35 -0800

    data update and fixing typos in tooltips glossary

--- glossary.js
+++ glossary.js
@@ -1,11 +1,11 @@
 module.exports = {
 	category: 'Every project gets a primary category based on its carbon removal mechanism. Click to toggle a category in the filter, or double click to select only that category. Most projects have a secondary category, which is included in searches.',
 	search: 'When searching projects, we include the project applicant, project location, all category tags, and any project-specific keywords. Note that search also depends on other filter selections.',
-	source: 'Our database is sourced from public project proposals submitted to various entities, such as coroporate purchases of carbon removal. You can filter by these sources.',
+	source: 'Our database is sourced from public project proposals submitted to various entities, such as corporate purchases of carbon removal. You can filter by these sources.',
 	permanence: 'The duration over which carbon storage can be reasonably assured. For biological systems this duration is typically limited by physical and socioeconomic risks, whereas for chemical or geological systems the duration is effectively permanent.',
 	volume: 'The total volume of carbon removal and/or avoided emissions achieved by the project. We interpret these as gross volumes. Net removal requires looking at negativity.',
 	rating: 'After analyzing each metric separately for validation, we combine into a single score on a scale from 1 to 5. Validation of mechanism, volume, negativity, or permanence, or a perfect score on additionality, each add 1 to the total.',
-	mechanism: 'Does the project remove carbon dioxide from the atmosphere, or does it avoid an emissions that would have otherwise occured? While both can have positive climate benefits, the difference matters for accounting purposes.',
+	mechanism: 'Does the project remove carbon dioxide from the atmosphere, or does it avoid emissions that would have otherwise occurred? While both can have positive climate benefits, the difference matters for accounting purposes.',
 	negativity: 'Negativity reflects the emissions intensity of different carbon removal solutions, and we define it as 1 minus the ratio of gross project emissions to gross climate benefits.',
 	additionality: 'Additionality refers to the causal relationship between the funds a climate project seeks and the climate benefits it claims. Establishing a connection depends on a counterfactual scenario that asks what would happen in the absence of funding.',
 	specificity: 'Specificity reflects whether there is enough detailed information in project proposals, publications, or other public materials for us to validate our metrics.'
