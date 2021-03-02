b2ddd95aeb8f536857a4b88c825958539eec41f5
freeman-lab <the.freeman.lab@gmail.com>
2021-03-02 13:59:56 -0800

    add option for just extracting numerical values

--- scripts/build_projects.py
+++ scripts/build_projects.py
@@ -11,7 +11,8 @@ import strp2020
 @click.argument('rfps', nargs=-1)
 @click.option('--output-projects', default='projects.js', show_default=True)
 @click.option('--output-methods', default='methods.js', show_default=True)
-def main(rfps, output_projects, output_methods):
+@click.option('--output-numbers', default='numbers.js', show_default=True)
+def main(rfps, output_projects, output_methods, output_numbers):
 
     projects = []
 
@@ -24,6 +25,7 @@ def main(rfps, output_projects, output_methods):
 
     write_projects(project_collection, output_projects)
     write_methods(project_collection, output_methods)
+    write_numbers(project_collection, output_numbers)
 
 def write_projects(project_collection, output):
     collection = copy.deepcopy(project_collection)
@@ -49,6 +51,26 @@ def write_methods(project_collection, output):
 
     with open('data/' + output, "w") as f:
         f.write('module.exports=' + json.dumps(collection))
+
+def write_numbers(project_collection, output):
+    collection = copy.deepcopy(project_collection)
+    for project in collection['projects']:
+        del project['methods']
+        del project['description']
+        del project['documentation']
+        del project['revisions']
+        del project['source']
+        del project['type']
+        del project['keywords']
+        for metric in project['metrics']:
+            del metric['comment']
+            del metric['notes']
+            del metric['units']
+            del metric['type']
+
+    with open('data/' + output, "w") as f:
+        f.write('module.exports=' + json.dumps(collection))
+
 
 if __name__ == "__main__":
     main()
