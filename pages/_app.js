a1b459a02f9b315e189176e397728126c5f6f1ed
freeman-lab <the.freeman.lab@gmail.com>
2021-12-15 10:35:07 -0800

    remove auth and update data

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,14 @@
 import { useState } from 'react'
 import { ThemeProvider } from 'theme-ui'
-import { SessionProvider } from '../lib/session'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
 import theme from '../theme'
 
 const App = ({ Component, pageProps }) => {
-  const [session, setSession] = useState({ token: null, username: null })
   return (
-    <SessionProvider session={session} setSession={setSession}>
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
