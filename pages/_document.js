07fb6aa2aab5162dafb7abc07dfeaafcce2fd21f
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-04 11:10:02 -0600

    keep tracking for now

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,14 @@
 import Document, { Html, Main, NextScript, Head } from 'next/document'
+import { Tracking } from '@carbonplan/components'
 import { InitializeColorMode } from 'theme-ui'
 
 class MyDocument extends Document {
   render() {
     return (
       <Html className='no-focus-outline'>
-        <Head />
+        <Head>
+          <Tracking id={process.env.GA_TRACKING_ID} />
+        </Head>
         <body>
           <InitializeColorMode />
           <Main />
