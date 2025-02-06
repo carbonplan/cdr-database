1f91cd8ff0c24e94639f4a1b041918d727f3c932
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-02-06 16:55:12 -0600

    use script

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PlausibleProvider from 'next-plausible'
+import Script from 'next/script'
 import { ThemeProvider } from 'theme-ui'
 import '@carbonplan/components/fonts.css'
 import '@carbonplan/components/globals.css'
@@ -7,16 +7,17 @@ import theme from '../theme'
 
 const App = ({ Component, pageProps }) => {
   return (
-    <PlausibleProvider
-      domain='carbonplan.org'
-      trackOutboundLinks
-      trackFileDownloads
-      trackLocalhost
-    >
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </PlausibleProvider>
+    <ThemeProvider theme={theme}>
+      {process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && (
+        <Script
+          strategy='lazyOnload'
+          data-domain='carbonplan.org'
+          data-api='https://carbonplan.org/proxy/api/event'
+          src='https://carbonplan.org/js/script.file-downloads.outbound-links.js'
+        />
+      )}
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
 }
 
